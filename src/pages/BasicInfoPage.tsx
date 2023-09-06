1982b1176221037f55cecc2b669f4c205f54c076
Evan <2248392717@qq.com>
2023-09-06 20:49:14 +0200

    finish the style

--- src/pages/BasicInfoPage.tsx
+++ src/pages/BasicInfoPage.tsx
@@ -1,7 +1,6 @@
 // src/pages/BasicInfoPage.tsx
-
-import React from 'react';
-import { Typography, Form, Input, Button, Select } from 'antd';
+import React, { useState } from 'react';
+import { Typography, Form, Input, Button, Select, Checkbox, Modal } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../UserContext';
 
@@ -12,10 +11,25 @@ const BasicInfoPage: React.FC = () => {
 
   const navigate = useNavigate();
   const { setUserInfo } = useUser();
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const onFinish = (values: any) => {
     console.log('Received values of form: ', values);
     setUserInfo(values); // <-- Update the global user info
     navigate('/image-comparison');
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
   };
 
   return (
@@ -38,7 +52,7 @@ const BasicInfoPage: React.FC = () => {
         <Form.Item
           name="age"
           label="Age"
-          rules={[{ required: true, message: 'Please input your age!' }]}
+          rules={[{ required: false, message: 'Please input your age!' }]}
         >
           <Input type="number" placeholder="25" />
         </Form.Item>
@@ -46,13 +60,29 @@ const BasicInfoPage: React.FC = () => {
         <Form.Item
           name="gender"
           label="Gender"
-          rules={[{ required: true, message: 'Please select your gender!' }]}
+          rules={[{ required: false, message: 'Please select your gender!' }]}
         >
           <Select placeholder="Select your gender">
             <Option value="male">Male</Option>
             <Option value="female">Female</Option>
+            <Option value="other">Non Binary</Option>
             <Option value="other">Other</Option>
           </Select>
+        </Form.Item>
+
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value ? Promise.resolve() : Promise.reject(new Error('You must agree to the terms.')),
+            },
+          ]}
+        >
+          <Checkbox>
+          I have read and agree to the <a onClick={showModal} className="underline text-blue-500 cursor-pointer">terms</a>.
+          </Checkbox>
         </Form.Item>
 
         <Form.Item>
@@ -60,6 +90,23 @@ const BasicInfoPage: React.FC = () => {
             Submit
           </Button>
         </Form.Item>
+
+
+      <Modal title="Terms and Conditions" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} className="text-justify" okButtonProps={{ style: { color: '#000' } }}>
+          <div className="space-y-2">
+            <p>I agree to participate in the user interface experiment by the User Interfaces group.</p>
+            <p>I have understood that the material and research data is gathered for scientific purposes only. The purpose and nature of the study have been explained to me in writing. I have sufficient information on the process of the study.</p>
+            <p>I understand that my participation in the study is completely voluntary and that I have the right to discontinue my participation at any stage without any consequences.</p>
+            <p>I give permission for my data to be recorded in the described manner.</p>
+            <p>I understand that I can ask to take a break at any time during the study.</p>
+            <p>It has been explained to me that a designated researcher will, at my request, provide me with additional details of the general principles of the study and its progress or of the results concerning myself.</p>
+            <p>I understand that anonymity will be ensured by disguising my identity. I have been explained who are the different parties involved in the research that have access to my data. I understand the practices of storing, protecting, and using the data.</p>
+            <p>I know that the collected data will not be presented to a third party without my written consent. I know that the research group may ask for a professional consultation on possible unexpected incidental findings without separate consent provided that the anonymity of the results has been ensured. Any type of commercial exploitation of the results is prohibited.</p>
+            <p>I understand that a fully anonymized subset of the data may be released to other research groups for the purposes mentioned above, if I give permission to it.</p>
+
+          </div>
+        </Modal>
+
       </Form>
     </div>
   );
--- src/pages/ImageComparisonPage.tsx
+++ src/pages/ImageComparisonPage.tsx
@@ -36,14 +36,52 @@ const ImageComparisonPage: React.FC = () => {
   
     setCurrentIndex((prevIndex) => prevIndex + 1);
   };
+
+  const handleSimilarChoice = () => {
+    const choiceData = {
+      name: imagePairs[currentIndex].name,
+      choiceName: 'similar',
+      grid: imagePairs[currentIndex].grid
+    };
+
+    setChoices((prevChoices) => {
+      const existingChoiceIndex = prevChoices.findIndex(
+        (choice) => choice.name === choiceData.name
+      );
+
+      if (existingChoiceIndex !== -1) {
+        const updatedChoices = [...prevChoices];
+        updatedChoices[existingChoiceIndex] = choiceData;
+        return updatedChoices;
+      }
+
+      return [...prevChoices, choiceData];
+    });
+
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
   
 
   const handlePrevious = () => {
     if (currentIndex > 0) {
       setCurrentIndex((prevIndex) => prevIndex - 1);
-      setChoices((prevChoices) => prevChoices.slice(0, -1));
+  
+      setChoices((prevChoices) => {
+        const existingChoiceIndex = prevChoices.findIndex(
+          (choice) => choice.name === imagePairs[currentIndex].name
+        );
+  
+        if (existingChoiceIndex !== -1) {
+          const updatedChoices = [...prevChoices];
+          updatedChoices.splice(existingChoiceIndex, 1);
+          return updatedChoices;
+        }
+  
+        return prevChoices;
+      });
     }
   };
+  
 
   // const handleSubmit = () => {
   //   console.log('User choices:', choices);
@@ -54,7 +92,7 @@ const ImageComparisonPage: React.FC = () => {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
         <h1>Thank you for participating!</h1>
-        <Button type="primary" onClick={submitTheAnswer}>
+        <Button type="primary" onClick={submitTheAnswer} className='text-black'>
           Submit Results
         </Button>
       </div>
@@ -90,29 +128,29 @@ const ImageComparisonPage: React.FC = () => {
         <Button onClick={handlePrevious} disabled={currentIndex === 0}>
           Previous
         </Button>
-        <Button onClick={() => handleChoice(-1)}>Skip</Button>
+        <Button onClick={() => setCurrentIndex((prevIndex) => prevIndex + 1)}>Skip</Button>
+        <Button onClick={handleSimilarChoice}>They look similar</Button> 
       </div>
       <Progress
         className="mt-4"
         percent={(currentIndex / imagePairs.length) * 100}
         showInfo={false}
       />
-      <div className="flex mt-4 flex-wrap">
-        {imagePairs.map((_, index) => (
-          <Tooltip title={`Image Pair ${index + 1}`} key={index}>
-            <div
-              className={`w-4 h-4 m-1 cursor-pointer text-xs flex items-center justify-center ${
-                choices.some(choice => choice.name === _.name)
-                  ? 'bg-blue-500 text-white'
-                  : 'bg-gray-300'
-              }`}
-              onClick={() => setCurrentIndex(index)}
-            >
-              {index + 1}
-            </div>
-          </Tooltip>
-        ))}
-      </div>
+    <div className="flex mt-4 flex-wrap">
+      {imagePairs.map((_, index) => (
+        <Tooltip title={`Image Pair ${index + 1}`} key={index}>
+          <div
+            className={`w-4 h-4 m-1 cursor-pointer text-xs flex items-center justify-center 
+              ${choices.some(choice => choice.name === _.name) ? 'bg-blue-500 text-white' : 'bg-gray-300'} 
+              ${index === currentIndex ? 'border-2 border-black' : ''}`} // Added condition for black border
+            onClick={() => setCurrentIndex(index)}
+          >
+            {index + 1}
+          </div>
+        </Tooltip>
+      ))}
+    </div>
+
       <Button className="mt-4" onClick={submitTheAnswer}>
         Submit Current Results
       </Button>
